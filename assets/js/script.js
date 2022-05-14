7013f61cdb3a7a46b8a7500e9a82d3ab709a8bd5
ClementNjeru <njeruclement36@gmailcom>
2022-05-14 21:06:30 +0300

    updates the Javascript page, the various function

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+const quizData = [
+    {
+Question1: "What is the short form of HTML?",
+a: html,
+b:hmlt,
+c:hmlt,
+d:lmth,
+
+correct: "a"},
+
+
+{Question2: "Which tag is used to write the javascript code?",
+ a:script,
+ b:sp,
+ c:javascript,
+ d:java,
+
+ correct: "a"},
+
+{Question3: "Which type of language is Javascript?", 
+ a:Programming,
+ b:Scripting,
+ c:Markup,
+ d:None,
+
+ correct: "b"},
+
+{Question4:"Which is not valid data type in Javascript", 
+a:Undefinded,
+b:Boolean,
+c:float,
+d:Number,
+
+correct: "c"},
+
+{Question5:"Upon encountering empty statements, what does the Javascript Interpreter do?",
+a:Throwserrors,
+b:Ignores,
+c:Warns,
+d:None,
+
+correct: "b"},
+    
+]
